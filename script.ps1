a8488614d2b6f7eaefdea5d80e6eabe928f11b8e
swamishreekant2 <swami.shreekant2@gmail.com>
2026-06-29 19:17:48 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -3,3 +3,7 @@ param(
 )
 
 Write-Output "SLACK_OUTPUT: Hello $Name"
+Write-Output "Server : SQL01"
+Write-Output "Database : TestDB"
+Write-Output "Status : Healthy"
+Write-Output "Backup : Successful"
